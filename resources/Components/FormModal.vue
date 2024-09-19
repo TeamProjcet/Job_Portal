b9046a21721f2c8b78e2e43f2dfa443abf545637
mohon sarker <mohonsarker40@gmail.com>
2024-09-19 11:32:57 +0600

    folder and files created inside resources

---
+++ resources/Components/FormModal.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "formModal"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ resources/Components/SideNav.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "sideNav"
+    }
+</script>
+
+<style scoped>
+
+</style>
