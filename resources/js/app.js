e44d8b2becd18e9e64b61e2fab608865868627bb
shimonahamed <smjibonahamed06@gmail.com>
2024-09-21 22:39:57 +0600

    app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -6,6 +6,30 @@ Vue.use(VueRouter);
 import App from './App.vue'
 import route from './Routes'
 
+import axios from 'axios';
+import vueAxios from 'vue-axios';
+Vue.use( vueAxios,axios);
+
+import httpMxin from './Mixin/httpMxin';
+import commonMixin from './Mixin/commonMixin';
+Vue.mixin(httpMxin);
+Vue.mixin(commonMixin);
+
+import Vuex from 'vuex'
+Vue.use(Vuex);
+
+import {store as storeData} from './Store';
+const store=new Vuex.Store(storeData);
+
+import veeValidate from 'vee-validate';
+Vue.use(veeValidate,{
+    events:'input',
+    fieldsBagName:''
+});
+
+
+
+
 const router = new VueRouter({
     mode: 'history',
     routes: route,
@@ -15,5 +39,5 @@ const router = new VueRouter({
 const vue = new Vue({
     el: '#app',
     components:{App},
-    router
+    router,axios,store
 });
