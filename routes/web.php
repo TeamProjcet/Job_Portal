4f503588e9b51c0523f7f888887c51be7fcfac83
shimonahamed <smjinonahamed06@gmail.com>
2024-09-22 16:34:58 +0600

    fix

--- routes/web.php
+++ routes/web.php
@@ -5,18 +5,18 @@ use Illuminate\Support\Facades\Route;
 
 //backend route
 //=============>
+Route::group(['middleware'=>'auth'],function (){
 
-Route::get('login', [\App\Http\Controllers\auth\AuthController::class, 'index']);
+});
+Route::get('login', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('login');
 Route::get('register', [\App\Http\Controllers\auth\AuthController::class, 'viewReg']);
 Route::resource('/userRegister', \App\Http\Controllers\auth\AuthController::class);
 Route::post('adlogin', [\App\Http\Controllers\auth\AuthController::class, 'adlogin']);
 Route::get('logout', [\App\Http\Controllers\auth\AuthController::class, 'logout']);
 
-Route::view('admin/{any}', 'backend')->where('any', '.*');
+Route::view('admin/{any}', 'backend')->where('any', '.*')->middleware('auth');
 
-
-//frontend route
-//==============>
 Route::view('/{any}', 'frontend')->where('any', '.*');
+
 
 
