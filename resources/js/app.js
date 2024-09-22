3fd3a4d7f8ac2c92b5894583625ad8e87c7d8dcf
RabbyHasanSaref <rabbyhasansaref83@gmail.com>
2024-09-22 21:04:08 +0600

    comment

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,39 +1,38 @@
+// vue import and use 
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
 Vue.use(VueRouter);
 
 import App from './App.vue'
+
+// vue routing path index.js 
 import route from './Routes'
 
+// api handling 
 import axios from "axios"
 import VueAxios from "vue-axios"
-
 Vue.use(VueAxios, axios)
 
+// common-function define and create 
 import httpMixin from "./Mixin/httpMxin"
 import commonMixin from "./Mixin/commonMixin"
-
 Vue.mixin(httpMixin)
 Vue.mixin(commonMixin)
 
+// store management 
 import Vuex from 'vuex';
-
 Vue.use(Vuex);
-
 import {store as storeData} from './Store';
-
 const store = new Vuex.Store(storeData);
 
-
+// input field global error handling 
 import VeeValidate from 'vee-validate'
-
 Vue.use(VeeValidate, {
     events: 'input',
     fieldsBagName: ''
 });
 
-
+// router mode history And hash#
 const router = new VueRouter({
     mode: 'history',
     routes: route,
@@ -42,14 +41,16 @@ const router = new VueRouter({
 
 
 
-
+// small common-function 
 // import helper from './helper'
 // helper(store, router);
 
+// alret massage 
 import Toast from 'vue-toastification';
 import 'vue-toastification/dist/index.css';
 Vue.use(Toast);
 
+// alret massage 
 import VueSweetalert2 from 'vue-sweetalert2';
 Vue.use(VueSweetalert2);
 import 'sweetalert2/dist/sweetalert2.min.css';
--- routes/web.php
+++ routes/web.php
@@ -4,18 +4,19 @@ use Illuminate\Support\Facades\Route;
 
 
 //backend route
-//=============>
-Route::group(['middleware'=>'auth'],function (){
 
-});
 Route::get('login', [\App\Http\Controllers\auth\AuthController::class, 'index'])->name('login');
 Route::get('logout', [\App\Http\Controllers\auth\AuthController::class, 'logout'])->name('logout');
 Route::get('register', [\App\Http\Controllers\auth\AuthController::class, 'viewReg']);
 Route::resource('/userRegister', \App\Http\Controllers\auth\AuthController::class);
 Route::post('adlogin', [\App\Http\Controllers\auth\AuthController::class, 'adlogin']);
-//Route::get('logout', [\App\Http\Controllers\auth\AuthController::class, 'logout']);
 
+Route::group(['middleware'=>'auth'],function (){
+
+});
 Route::view('admin/{any}', 'backend')->where('any', '.*')->middleware('auth');
+
+//frontend Route
 
 Route::view('/{any}', 'frontend')->where('any', '.*');
 
